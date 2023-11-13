61d343888916cc150ee9fe178058b05e9c3f3e32
firecarlstream <blueskyfred@163.com>
2023-11-13 15:49:33 +0800

    添加网络层代码

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -0,0 +1,18 @@
+package com.sunnyweather.android.logic.network
+
+import com.sunnyweather.android.SunnyWeatherApplication
+import com.sunnyweather.android.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author zhangkai
+ * @description:
+ * @date: 2023/11/13 15:00
+ */
+interface PlaceService {
+
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+}
