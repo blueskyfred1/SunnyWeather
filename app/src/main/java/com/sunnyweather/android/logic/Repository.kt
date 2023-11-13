5dcb8270b42a33e0616e02f1a5d21b990cde2adb
zhangkai <945018084@qq.com>
2023-11-13 16:44:56 +0800

    添加仓库封装层

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,29 @@
+package com.sunnyweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+/**
+ * @author zhangkai
+ * @description:封装仓库层
+ * @date: 2023/11/13 16:17
+ */
+object Repository {
+
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            }else{
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        }catch (e: Exception) {
+
+        }
+        emit(result)
+    }
+}
